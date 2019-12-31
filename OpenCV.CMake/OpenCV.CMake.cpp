b89a2ab94ec4630e99a40ba12ee7eaef722dbd87
Michael Yarichuk <michael.yarichuk@gmail.com>
2019-12-31 10:50:12 +0200

    stuff compile and build

---
+++ OpenCV.CMake/OpenCV.CMake.cpp
@@ -0,0 +1,15 @@
+﻿// OpenCV.CMake.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+#include <opencv2/imgcodecs.hpp>
+
+
+using namespace std;
+
+int main()
+{
+	const auto img = cv::imread("e:\\Capture.JPG");
+	cout << "image size: " << img.cols << "x" << img.rows << endl;
+	return 0;
+}
